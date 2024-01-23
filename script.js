0dab9312010f3fea83f36c115719eced04504269
agira-siva <sivaramakrishnan.s@agiratech.com>
2024-01-23 10:42:19 +0530

    thirdepisode code

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [React.createElement(
-    "div",
-    { id: "child"},
-    [React.createElement("h1", {}, "i am nested siva"),React.createElement("h2", {}, "i am nested ashok")]
-  ),React.createElement(
-    "div",
-    { id: "child"},
-    [React.createElement("h1", {}, "i am nested siva"),React.createElement("h2", {}, "i am nested ashok")]
-  )]
-);
-
-console.log(parent);
-const heading = React.createElement("h1", {}, "hii i am siva from react");
 const body = document.getElementById("container");
-const reactRoot = ReactDOM.createRoot(body);
-reactRoot.render(parent);
+const root = ReactDOM.createRoot(body);
+const ButtonComponent = () => 
+    (
+      <>
+        {jsxHeading}
+        <Heading />
+        <Paragraph/>
+        <button className="siva">click me</button>
+      </>
+    );
+
+
+const Heading = () => React.createElement("h1", {}, "hii i am siva from react");
+
+function Paragraph(){
+    return <p>click the below button to know more about me</p>
+}
+
+root.render(<ButtonComponent/>);
+const jsxHeading = <h1>i am siva from jsx</h1>;
+
+
+
+
 
